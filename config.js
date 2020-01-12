2547dc76bc12d4969b6792be712f72a8986be84a
Kraft Pavel <pykraft@gmail.com>
2020-01-12 19:03:15 +0200

    connect to MongoDB Atlas

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	PORT: process.env.PORT || 80,
-	MONGO_URL: 'mongodb://localhost:27017/users_db',
+	// MONGO_URL: 'mongodb://localhost:27017/users_db',
+	MONGO_URL:
+		'mongodb+srv://dbUser:userpass@kraftdb-eonvl.gcp.mongodb.net/test?retryWrites=true&w=majority',
 	MONGO_OPTIONS: {
 		useUnifiedTopology: true,
 		useNewUrlParser: true
